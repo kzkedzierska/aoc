921a2b7d0784276d0e3ec23e29acddbeebe9b0b4
kzkedzierska <kzkedzierska@protonmail.com>
2022-12-19 00:51:45 +0000

    solved Day 10 and part one of Day 18, changed stars generation to account for patchy solving

--- 2022/misc/generate_input_for_summary_table.sh
+++ 2022/misc/generate_input_for_summary_table.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # get all day headers
-grep "^## Day" answers.qmd |
-  sed 's/## //g' | 
+grep -E "^## Day|^### Part" answers.qmd |
+  sed 's/## //g; s/### //g' | 
     tr '[:upper:]' '[:lower:]' | 
       sed 's/://g; s/ /-/g; s/^/#/g' > misc/solved_days
 
-# get all part headers
-grep "^### Part " answers.qmd  | 
-  cut -f4 -d' ' > misc/stars
+# # get all part headers
+# grep "^### Part " answers.qmd  | 
+#   cut -f4 -d' ' > misc/stars
