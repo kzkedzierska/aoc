8cfb5aa30a5179fb8a72c56583904b9b81aad9b6
kzkedzierska <31040515+kzkedzierska@users.noreply.github.com>
2024-12-03 16:36:35 +0000

    :sparkles: added bash solutions

---
+++ 2024/misc/helper.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#==============================================================================
+# Description:   Helper fuctions for the Advent of Code 2024
+# Author:        Kasia Kedzierska
+# Date:          December 3rd, 2024
+#==============================================================================
+
+# Function to check if a test passed
+test_check() {
+  local actual=$1
+  local expected=$2
+  local comment=$3
+  local msg
+
+  if [ "${actual}" -ne "${expected}" ]; then
+    msg="${comment} Test failed: ${actual} != ${expected}"
+    command -v cowsay >/dev/null &&
+      cowsay -f mutilated "${msg}" ||
+      echo "❌ ${msg}"
+    echo "Test failed, exiting..."
+    exit 1
+  else
+    msg="${comment} test passed!"
+    command -v cowsay >/dev/null &&
+      cowsay -f tux "${msg}" ||
+      echo "✔️ ${msg}"
+  fi
+}
+
+# Print the answer
+print_answer() {
+  local answer=$1
+  local part=$2
+  local msg="Part ${part} answer is: ${answer}"
+  command -v cowsay >/dev/null &&
+    cowsay -f default "${msg}" ||
+    echo -e "${msg}"
+}
